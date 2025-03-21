a908a4a5ea594de28656c7feffdc0aace3d41e78
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2025-03-22 00:53:08 +0530

    Converted the main row in pull_stock function to a numpy list from a native python list to increase performance

--- data-collection/pull_stock.py
+++ data-collection/pull_stock.py
@@ -1,5 +1,5 @@
 import yfinance as yf
-
+import numpy as np
 
 # Pulls Stock Data
 def get_stock_data(ticker):
@@ -7,21 +7,23 @@ def get_stock_data(ticker):
     stock_history = stock.history(period="1d")
 
     # This will be the row that we will eventually end up adding to the main csv file
-    row = []
+    row = np.ndarray(110)
     
     # Appending stock history related values to row
-    row.append(stock_history["Close"][0]) # Adding Closing Price
-    row.append(stock_history["Open"][0]) # Adding Openeing Price
-    row.append(stock_history["High"][0]) # Adding High
-    row.append(stock_history["Low"][0]) # Adding Low
-    row.append(stock_history["Volume"][0]) # Adding Volume
+    # row.append(stock_history["Close"][0]) # Adding Closing Price
+    row[0] = stock_history["Close"][0] # Adding Closing Price
+    row[1] = stock_history["Open"][0] # Adding Openeing Price
+    row[2] = stock_history["High"][0] # Adding High
+    row[3] = stock_history["Low"][0] # Adding Low
+    row[4] = stock_history["Volume"][0] # Adding Volume
 
     # Keys for the info dictionary object in Ticker that will be sued for extracting numerical data
-    info_keys = ["forwardPE", "fullTimeEmployees", "auditRisk", "boardRisk", "compensationRisk","shareHolderRightsRisk", "overallRisk", "governanceEpochDate", "compensationAsOfEpochDate","maxAge", "priceHint", "previousClose", "dayLow", "dayHigh", "regularMarketPreviousClose","regularMarketOpen", "regularMarketDayLow", "regularMarketDayHigh", "dividendRate","dividendYield", "exDividendDate", "payoutRatio", "fiveYearAvgDividendYield", "beta","trailingPE", "volume", "regularMarketVolume", "averageVolume", "averageVolume10days","averageDailyVolume10Day", "marketCap", "fiftyTwoWeekLow", "fiftyTwoWeekHigh","priceToSalesTrailing12Months", "fiftyDayAverage", "twoHundredDayAverage","trailingAnnualDividendRate", "trailingAnnualDividendYield", "enterpriseValue","profitMargins", "floatShares", "sharesOutstanding", "heldPercentInsiders","heldPercentInstitutions", "impliedSharesOutstanding", "bookValue", "priceToBook","lastFiscalYearEnd", "nextFiscalYearEnd", "mostRecentQuarter", "earningsQuarterlyGrowth","netIncomeToCommon", "trailingEps", "forwardEps", "lastSplitDate","enterpriseToRevenue", "enterpriseToEbitda", "fiftyTwoWeekChange", "SandP52WeekChange","lastDividendValue", "lastDividendDate", "currentPrice", "targetHighPrice", "targetLowPrice","targetMeanPrice", "targetMedianPrice", "recommendationMean", "numberOfAnalystOpinions","totalCash", "totalCashPerShare", "ebitda", "totalDebt", "totalRevenue", "debtToEquity","revenuePerShare", "grossProfits", "earningsGrowth", "revenueGrowth", "grossMargins","ebitdaMargins", "operatingMargins", "firstTradeDateMilliseconds", "regularMarketChange","averageDailyVolume3Month", "fiftyTwoWeekLowChange", "fiftyTwoWeekLowChangePercent","fiftyTwoWeekHighChange", "fiftyTwoWeekHighChangePercent", "fiftyTwoWeekChangePercent","earningsTimestamp", "earningsTimestampStart", "earningsTimestampEnd", "epsTrailingTwelveMonths","epsForward", "epsCurrentYear", "priceEpsCurrentYear", "fiftyDayAverageChange","fiftyDayAverageChangePercent", "twoHundredDayAverageChange", "twoHundredDayAverageChangePercent","sourceInterval", "exchangeDataDelayedBy", "regularMarketTime", "gmtOffSetMilliseconds","regularMarketChangePercent", "regularMarketPrice"]
+    info_keys = ["forwardPE", "fullTimeEmployees", "auditRisk", "boardRisk", "compensationRisk","shareHolderRightsRisk", "overallRisk", "governanceEpochDate", "compensationAsOfEpochDate","maxAge", "priceHint", "previousClose", "dayLow", "dayHigh", "regularMarketPreviousClose","regularMarketOpen", "regularMarketDayLow", "regularMarketDayHigh", "dividendRate","dividendYield", "exDividendDate", "payoutRatio", "fiveYearAvgDividendYield", "beta","trailingPE", "volume", "regularMarketVolume", "averageVolume", "averageVolume10days","averageDailyVolume10Day", "marketCap", "fiftyTwoWeekLow", "fiftyTwoWeekHigh","priceToSalesTrailing12Months", "fiftyDayAverage", "twoHundredDayAverage","trailingAnnualDividendRate", "trailingAnnualDividendYield", "enterpriseValue","profitMargins", "floatShares", "sharesOutstanding", "heldPercentInsiders","heldPercentInstitutions", "impliedSharesOutstanding", "bookValue", "priceToBook","lastFiscalYearEnd", "nextFiscalYearEnd", "mostRecentQuarter", "earningsQuarterlyGrowth","netIncomeToCommon", "trailingEps", "forwardEps", "lastSplitDate","enterpriseToRevenue", "enterpriseToEbitda", "SandP52WeekChange","lastDividendValue", "lastDividendDate", "currentPrice", "targetHighPrice", "targetLowPrice","targetMeanPrice", "targetMedianPrice", "recommendationMean", "numberOfAnalystOpinions","totalCash", "totalCashPerShare", "ebitda", "totalDebt", "totalRevenue", "debtToEquity","revenuePerShare", "grossProfits", "earningsGrowth", "revenueGrowth", "grossMargins","ebitdaMargins", "operatingMargins", "firstTradeDateMilliseconds", "regularMarketChange","averageDailyVolume3Month", "fiftyTwoWeekLowChange", "fiftyTwoWeekLowChangePercent","fiftyTwoWeekHighChange", "fiftyTwoWeekHighChangePercent", "fiftyTwoWeekChangePercent","earningsTimestamp", "earningsTimestampStart", "earningsTimestampEnd", "epsTrailingTwelveMonths","epsForward", "epsCurrentYear", "priceEpsCurrentYear", "fiftyDayAverageChange","fiftyDayAverageChangePercent", "twoHundredDayAverageChange", "twoHundredDayAverageChangePercent","sourceInterval", "exchangeDataDelayedBy", "regularMarketTime", "gmtOffSetMilliseconds","regularMarketChangePercent", "regularMarketPrice"]
 
     # Adding data thorugh keys into the row
-    for key in info_keys:
-        row.append(stock.info.get(key))
+    for index, key in enumerate(info_keys):
+        # row.append(np.float64(stock.info.get(key)))
+        row[5+index] = np.float64(stock.info.get(key))
 
     return row
 
